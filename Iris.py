f9f081063555261511ca50c45ff80a937dbc09c7
Lyy <liyunyao1010@163.com>
2018-04-21 14:09:35 +0800

    update

--- Iris.py
+++ Iris.py
@@ -2,11 +2,12 @@
 #including modole data 
 class IrisModel(object):
     def __init__(self, label_n, item_n):
-        self.P_c = []    #P(c)
-        self.D_c = []    #D(c)
-        self.D   = 0     #data num
-        self.P_x_c = []  #P(xi|c)
+        self.P_c = []    #P(c)相关类别的先验概率列表
+        self.D_c = []    #D(c)相关类别的训练样本总量
+        self.D   = 0     #data num训练样本总量
+        self.P_x_c = []  #P(xi|c)属性基于类别的后验概率的模型值,连续函数为正态分布的两个参数
         n = label_n
+        #初始化相关参数
         while(n):
             self.P_c.append(0)
             self.D_c.append(0)
